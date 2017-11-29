ee7c85eae38fdad8bbfc64a83a64cb3cdb462f0f
Nick.Lin <tick_lin@sina.com>
2017-11-29 17:59:18 +0800

    fix issue of running dev fail

--- template/config/index.js
+++ template/config/index.js
@@ -3,7 +3,7 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 
 const path = require('path')
-var target = process.env.TARGET;
+var target = process.env.TARGET || "";
 
 module.exports = {
   dev: {

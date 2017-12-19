3e1186fb263463f3d4ee3707d2c703c5a608518c
Nick.Lin <tick_lin@sina.com>
2017-12-19 15:55:27 +0800

    update(test):单元测试模版内的代码改为双引号；

--- template/test/unit/specs/HelloWorld.spec.js
+++ template/test/unit/specs/HelloWorld.spec.js
@@ -1,11 +1,11 @@
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld';
+import Vue from "vue";
+import HelloWorld from "@/components/HelloWorld";
 /* global describe, it, expect */
 
-describe('HelloWorld.vue', () => {
-    it('should render correct contents', () => {
+describe("HelloWorld.vue", () => {
+    it("should render correct contents", () => {
         const Constructor = Vue.extend(HelloWorld);
         const vm = new Constructor().$mount();
-        expect(vm.$el.querySelector('.hello h1').textContent){{#if_eq runner "karma"}}.to.equal('Welcome to Your Vue.js App'){{/if_eq}}{{#if_eq runner "jest"}}.toEqual('Welcome to Your Vue.js App'){{/if_eq}};
+        expect(vm.$el.querySelector(".hello h1").textContent){{#if_eq runner "karma"}}.to.equal("Welcome to Your Vue.js App"){{/if_eq}}{{#if_eq runner "jest"}}.toEqual("Welcome to Your Vue.js App"){{/if_eq}};
     });
 });
